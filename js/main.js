cb7ee98c5e30853bfe020f2206bd6b7f9a970f68
Ndn1618 <ndilbar1618@gmail.com>
2020-10-10 09:20:27 +0500

    Make card to rotate with JS code

--- js/main.js
+++ js/main.js
@@ -7,13 +7,19 @@ elSiteHeaderToggler.addEventListener('click', function () {
   elSiteHeader.classList.toggle('site-header--open');
 });
 
-
 elSiteMainTogglers.forEach(function (elSiteMainToggler) {
   elSiteMainToggler.addEventListener('click', function () {
     elSiteMainToggler.classList.toggle('director__card-toggle--close');
-    elSiteMains.forEach(function (elSiteMain) {
-      elSiteMain.classList.toggle('director--open');
-    });
   });
 });
 
+elSiteMainTogglers.forEach(function (elSiteMainToggler, index) {
+  elSiteMainToggler.addEventListener('click', function () {
+    console.log(index)
+    elSiteMains.forEach(function (elSiteMain, number) {
+      if (index == number) {
+        elSiteMain.classList.toggle('director--open');
+      }
+    });
+  });
+});
