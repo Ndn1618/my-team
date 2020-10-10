26ef1d895552f8292976f97692de4dc7c545a4e4
Ndn1618 <ndilbar1618@gmail.com>
2020-10-10 08:00:42 +0500

    Add director section styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 var elSiteHeader = document.querySelector('.site-header');
 var elSiteHeaderToggler = document.querySelector('.site-header__nav-toggler');
+var elSiteMain = document.querySelector('.director');
+var elSiteMainToggler = document.querySelector('.director__card-toggle');
 
 elSiteHeaderToggler.addEventListener('click', function () {
   elSiteHeader.classList.toggle('site-header--open');
+});
+
+elSiteMainToggler.addEventListener('click', function () {
+  elSiteMain.classList.toggle('director--open');
+});
+
+elSiteMainToggler.addEventListener('click', function () {
+  elSiteMainToggler.classList.toggle('director__card-toggle--close');
 });
